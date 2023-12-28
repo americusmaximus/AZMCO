5350a65b507dee805386c57d655f24b3a5d16539
Americus Maximus <americancodemax@gmail.com>
2023-12-28 18:34:40 -0500

    Implementation of DirectX 8 renderers.

---
+++ Source/R.DirectX.8.0.A/Module.cxx
@@ -0,0 +1,419 @@
+/*
+Copyright (c) 2023 Americus Maximus
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "Module.hxx"
+
+using namespace Renderer;
+
+namespace RendererModule
+{
+    // 0x60001000
+    // a.k.a. THRASH_about
+    DLLAPI RendererModuleDescriptor* STDCALLAPI AcquireDescriptor(void)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return NULL;
+    }
+
+    // 0x60001390
+    // a.k.a. THRASH_clearwindow
+    DLLAPI u32 STDCALLAPI ClearGameWindow(void)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x600014c0
+    // a.k.a. THRASH_clip
+    DLLAPI u32 STDCALLAPI ClipGameWindow(const u32 x0, const u32 y0, const u32 x1, const u32 y1)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60001760
+    // a.k.a. THRASH_createwindow
+    // NOTE: Never being called by the application.
+    DLLAPI u32 STDCALLAPI CreateGameWindow(const u32 width, const u32 height, const u32 format, const u32 options)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60001810
+    // a.k.a. THRASH_destroywindow
+    // NOTE: Never being called by the application.
+    DLLAPI u32 STDCALLAPI DestroyGameWindow(const u32 indx)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60002940
+    // a.k.a. THRASH_drawline
+    DLLAPI void STDCALLAPI DrawLine(RVX* a, RVX* b)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x60002a70
+    // a.k.a. THRASH_drawlinemesh
+    DLLAPI void STDCALLAPI DrawLineMesh(const u32 count, RVX* vertexes, const u32* indexes)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x60002e70
+    // a.k.a. THRASH_drawlinestrip
+    DLLAPI void STDCALLAPI DrawLineStrip(const u32 count, RVX* vertexes)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x60002af0
+    // a.k.a. THRASH_drawlinestrip
+    // NOTE: Never being called by the application.
+    DLLAPI void STDCALLAPI DrawLineStrips(const u32 count, RVX* vertexes, const u32* indexes)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x60002c40
+    // a.k.a. THRASH_drawpoint
+    DLLAPI void STDCALLAPI DrawPoint(RVX* vertex)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x60002d40
+    // a.k.a. THRASH_drawpointmesh
+    DLLAPI void STDCALLAPI DrawPointMesh(const u32 count, RVX* vertexes, const u32* indexes)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x60002ea0
+    // a.k.a. THRASH_drawpointstrip
+    DLLAPI void STDCALLAPI DrawPointStrip(const u32 count, RVX* vertexes)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x600026b0
+    // a.k.a. THRASH_drawquad
+    DLLAPI void STDCALLAPI DrawQuad(RVX* a, RVX* b, RVX* c, RVX* d)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x60002890
+    // a.k.a. THRASH_drawquadmesh
+    DLLAPI void STDCALLAPI DrawQuadMesh(const u32 count, RVX* vertexes, const u32* indexes)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x60001900
+    // a.k.a. THRASH_drawsprite
+    // NOTE: Never being called by the application.
+    DLLAPI void STDCALLAPI DrawSprite(RVX* a, RVX* b)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x60001980
+    // a.k.a. THRASH_drawspritemesh
+    // NOTE: Never being called by the application.
+    DLLAPI void STDCALLAPI DrawSpriteMesh(const u32 count, RVX* vertexes, const u32* indexes)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x60002200
+    // a.k.a. THRASH_drawtri
+    DLLAPI void STDCALLAPI DrawTriangle(RVX* a, RVX* b, RVX* c)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x60002e40
+    // a.k.a. THRASH_drawtrifan
+    DLLAPI void STDCALLAPI DrawTriangleFan(const u32 count, RVX* vertexes)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x60002580
+    // a.k.a. THRASH_drawtrifan
+    DLLAPI void STDCALLAPI DrawTriangleFans(const u32 count, RVX* vertexes, const u32* indexes)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x600023b0
+    // a.k.a. THRASH_drawtrimesh
+    DLLAPI void STDCALLAPI DrawTriangleMesh(const u32 count, RVX* vertexes, const u32* indexes)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x60002df0
+    // a.k.a. THRASH_drawtristrip
+    DLLAPI void STDCALLAPI DrawTriangleStrip(const u32 count, RVX* vertexes)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x60002450
+    // a.k.a. THRASH_drawtristrip
+    DLLAPI void STDCALLAPI DrawTriangleStrips(const u32 count, RVX* vertexes, const u32* indexes)
+    {
+        // TODO NOT IMPLEMENTED
+    }
+
+    // 0x600013d0
+    // a.k.a. THRASH_flushwindow
+    DLLAPI u32 STDCALLAPI FlushGameWindow(void)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+    
+    // 0x60002f30
+    // a.k.a. THRASH_getstate
+    DLLAPI u32 STDCALLAPI AcquireState(const u32 state)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x600017f0
+    // a.k.a. THRASH_getwindowtexture
+    DLLAPI RendererTexture* STDCALLAPI AcquireGameWindowTexture(const u32 indx)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return NULL;
+    }
+
+    // 0x60008750
+    // a.k.a. THRASH_idle
+    DLLAPI void STDCALLAPI Idle(void) { return; }
+
+    // 0x600030f0
+    // a.k.a. THRASH_init
+    DLLAPI u32 STDCALLAPI Init(void)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60004a70
+    // a.k.a. THRASH_is
+    DLLAPI u32 STDCALLAPI Is(void)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60001590
+    // a.k.a. THRASH_lockwindow
+    DLLAPI RendererModuleWindowLock* STDCALLAPI LockGameWindow(void)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return NULL;
+    }
+
+    // 0x600013e0
+    // a.k.a. THRASH_pageflip
+    DLLAPI u32 STDCALLAPI ToggleGameWindow(void)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60002ed0
+    // a.k.a. THRASH_readrect
+    DLLAPI u32 STDCALLAPI ReadRectangle(const u32 x, const u32 y, const u32 width, const u32 height, u32* pixels)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60004a80
+    // a.k.a. THRASH_readrect
+    // NOTE: Never being called by the application.
+    DLLAPI u32 STDCALLAPI ReadRectangles(const u32 x, const u32 y, const u32 width, const u32 height, u32* pixels, const u32 stride)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60003380
+    // a.k.a. THRASH_restore
+    DLLAPI u32 STDCALLAPI RestoreGameWindow(void)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60003180
+    // a.k.a. THRASH_selectdisplay
+    DLLAPI u32 STDCALLAPI SelectDevice(const s32 indx)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60004bc0
+    // a.k.a. THRASH_setstate
+    DLLAPI u32 STDCALLAPI SelectState(const u32 state, void* value)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x600019f0
+    // a.k.a. THRASH_settexture
+    DLLAPI u32 STDCALLAPI SelectTexture(RendererTexture* tex)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60003230
+    // a.k.a. THRASH_setvideomode
+    DLLAPI u32 STDCALLAPI SelectVideoMode(const u32 mode, const u32 pending, const u32 depth)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60001450
+    // a.k.a. THRASH_sync
+    DLLAPI u32 STDCALLAPI SyncGameWindow(const u32 type)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60006d90
+    // a.k.a. THRASH_talloc
+    DLLAPI RendererTexture* STDCALLAPI AllocateTexture(const u32 width, const u32 height, const u32 format, const u32 options, const u32 state)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return NULL;
+    }
+
+    // 0x60006f20
+    // a.k.a. THRASH_tfree
+    DLLAPI u32 STDCALLAPI ReleaseTexture(RendererTexture* tex)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60007050
+    // a.k.a. THRASH_treset
+    DLLAPI u32 STDCALLAPI ResetTextures(void)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60006fb0
+    // a.k.a. THRASH_tupdate
+    DLLAPI RendererTexture* STDCALLAPI UpdateTexture(RendererTexture* tex, const u32* pixels, const u32* palette)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return NULL;
+    }
+
+    // 0x60006ff0
+    // a.k.a. THRASH_tupdaterect
+    DLLAPI RendererTexture* STDCALLAPI UpdateTextureRectangle(RendererTexture* tex, const u32* pixels, const u32* palette, const s32 x, const s32 y, const s32 width, const s32 height, const s32 size, const s32 level)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+    
+    // 0x600016f0
+    // a.k.a. THRASH_unlockwindow
+    DLLAPI u32 STDCALLAPI UnlockGameWindow(const RendererModuleWindowLock* state)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60001160
+    // a.k.a. THRASH_window
+    DLLAPI u32 STDCALLAPI SelectGameWindow(const u32 indx)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60002f00
+    // a.k.a. THRASH_writerect
+    DLLAPI u32 STDCALLAPI WriteRectangle(const u32 x, const u32 y, const u32 width, const u32 height, const u32* pixels)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+
+    // 0x60004b20
+    // a.k.a. THRASH_writerect
+    // NOTE: Never being called by the application.
+    DLLAPI u32 STDCALLAPI WriteRectangles(const u32 x, const u32 y, const u32 width, const u32 height, const u32* pixels, const u32 stride)
+    {
+        // TODO NOT IMPLEMENTED
+
+        return RENDERER_MODULE_FAILURE;
+    }
+}
