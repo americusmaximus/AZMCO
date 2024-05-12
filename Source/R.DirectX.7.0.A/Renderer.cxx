c46d11ec8121858893d4d645af34ae29aae6530e
Americus Maximus <americancodemax@gmail.com>
2024-05-12 09:21:44 -0400

    Improvements.

--- Source/R.DirectX.7.0.A/Renderer.cxx
+++ Source/R.DirectX.7.0.A/Renderer.cxx
@@ -2517,7 +2517,7 @@ namespace RendererModule
             {
             case RENDERER_MODULE_CULL_NONE:
             {
-                State.Settings.Cull = 1; // TODO
+                State.Settings.Cull = RENDERER_CULL_MODE_NONE;
 
                 SelectRendererStateValue(state, (void*)RENDERER_MODULE_CULL_NONE);
 
@@ -2525,7 +2525,7 @@ namespace RendererModule
             }
             case RENDERER_MODULE_CULL_COUNTER_CLOCK_WISE:
             {
-                State.Settings.Cull = 0x80000000; // TODO
+                State.Settings.Cull = RENDERER_CULL_MODE_COUNTER_CLOCK_WISE;
 
                 SelectRendererStateValue(state, (void*)RENDERER_MODULE_CULL_COUNTER_CLOCK_WISE);
 
@@ -2533,7 +2533,7 @@ namespace RendererModule
             }
             case RENDERER_MODULE_CULL_CLOCK_WISE:
             {
-                State.Settings.Cull = 0; // TODO
+                State.Settings.Cull = RENDERER_CULL_MODE_CLOCK_WISE;
 
                 SelectRendererStateValue(state, (void*)RENDERER_MODULE_CULL_CLOCK_WISE);
 
@@ -3869,7 +3869,7 @@ namespace RendererModule
             RVX* c = (RVX*)((addr)vertexes + (addr)(RendererVertexSize * ic));
             RVX* d = (RVX*)((addr)vertexes + (addr)(RendererVertexSize * id));
 
-            if (State.Settings.Cull == 1 || ((u32)AcquireNormal((f32x3*)a, (f32x3*)b, (f32x3*)c) & 0x80000000) != State.Settings.Cull) { RenderQuad(a, b, c, d); } // TODO
+            if (State.Settings.Cull == RENDERER_CULL_MODE_NONE || (AcquireNormal((f32x3*)a, (f32x3*)b, (f32x3*)c) & RENDERER_CULL_MODE_COUNTER_CLOCK_WISE) != State.Settings.Cull) { RenderQuad(a, b, c, d); }
         }
     }
 
@@ -3987,7 +3987,7 @@ namespace RendererModule
             RVX* b = (RVX*)((addr)vertexes + (addr)(RendererVertexSize * ib));
             RVX* c = (RVX*)((addr)vertexes + (addr)(RendererVertexSize * ic));
 
-            if (State.Settings.Cull == 1 || ((u32)AcquireNormal((f32x3*)a, (f32x3*)b, (f32x3*)c) & 0x80000000) != State.Settings.Cull) { RenderTriangle(a, b, c); } // TODO
+            if (State.Settings.Cull == RENDERER_CULL_MODE_NONE || (AcquireNormal((f32x3*)a, (f32x3*)b, (f32x3*)c) & RENDERER_CULL_MODE_COUNTER_CLOCK_WISE) != State.Settings.Cull) { RenderTriangle(a, b, c); }
         }
     }
 
